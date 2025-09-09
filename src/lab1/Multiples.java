5751636258fb49db99d789c92a42afcc6bbe419b
John Lin <john.lin3955@gmail.com>
2025-09-09 03:02:51 -0400

    refactored multiples into method

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,6 +2,11 @@ package lab1;
 
 public class Multiples {
     public static void main(String[] args) {
+        int c = multiples();
+        System.out.println(c);
+    }
+
+    public static int multiples() {
         int counter = 0;
         for(int i = 0; i < 1000; ++i) {
             if (i % 3 == 0 || i % 5 == 0) {
@@ -9,6 +14,6 @@ public class Multiples {
             }
         }
 
-        System.out.println(counter);
+        return counter;
     }
 }
