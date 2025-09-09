3b0467bd517a64ec5854a3001d9e602b68242b5b
John Lin <john.lin3955@gmail.com>
2025-09-09 03:52:25 -0400

    fix

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -8,7 +8,7 @@ public class Multiples {
 
     public static int multiples(int n, int a, int b) {
         int counter = 0;
-        for(int i = 0; i < n; ++i) {
+        for(int i = 1; i < n; ++i) {
             if (i % a == 0 || i % b == 0) {
                 counter++;
             }
